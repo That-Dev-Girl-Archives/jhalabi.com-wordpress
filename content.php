a3b0e18d91951fd73a154763031303cd91aed0e0
Joni <joni@jhalabi.com>
2015-07-24 07:38:01 -0400

    Bug fixes
    Fixing issue with latest post appearing on archive pages, updating
    header styles on tag pages

--- content.php
+++ content.php
@@ -6,7 +6,11 @@
 
 <article id="post-<?php the_ID(); ?>" class="excerpt__article">
 	<header>
-		<?php the_title( sprintf( '<h2><b>Latest Post:</b> <a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php if (is_archive()):?>
+			<?php the_title( sprintf( '<h2><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php else: ?>
+			<?php the_title( sprintf( '<h2><b>Latest Post:</b> <a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php endif; ?>
 
 		<div class="entry-meta">
 			<?php jhalabi_posted_on(); ?>
