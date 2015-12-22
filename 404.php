2100e56ef49732203b5801d9269f3c6ce014d51a
Joni Halabi <joni@jhalabi.com>
2015-12-21 23:52:15 -0500

    Updating design for 404 page.

--- 404.php
+++ 404.php
@@ -11,17 +11,17 @@ get_header(); ?>
 	<h1 class="columns">Congratulations!</h1>
 </header>
 
-<section class="error-404 not-found">
-	<h2>You did it!</h2>
-	<p>You found Dino's hiding place!  Dino has been sitting here <i>all day</i> waiting for you to stop by.</p>
-	<p>Dino is a great hider, but you are clearly a better seeker.  You rock!</p>
+<section class="error-404">
+	<div class="error-404__div">
+		<p>You found Dino's hiding place!  Dino has been sitting here <i>all day</i> waiting for you to stop by.</p>
+		<p>Dino is a great hider, but you are clearly a better seeker.  You rock!</p>
+	</div>
 
-	<p>
-		<br />
-		<img src="<?php echo get_template_directory_uri(); ?>/img/404-dino.png" />
-		<br />
-	</p>
+	<div class="error-404__div--dino">
+	</div>
+</section>
 
+<section class="error-404">
 	<h2>Wait... what?!</h2>
 	<p>What do you mean you were looking for something else?  Are you <i>sure</i> you weren't looking for Dino?</p>
 	<p>Ok, fine.  Why don't you use this search box to find whatever could possibly be more important than a cute, cuddly baby dinosaur.  Just don't tell Dino.  That would make him sad.</p>
