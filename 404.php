3631531872855c2ee46a92995af392c93a5a477e
Joni Halabi <her@thatdevgirl.com>
2017-05-08 23:32:00 -0400

    Updates to 404 page

--- 404.php
+++ 404.php
@@ -11,26 +11,33 @@ get_header(); ?>
 	<h1 class="columns">Congratulations!</h1>
 </header>
 
-<section class="error-404 error-404--bg">
+<section class="error-404">
 	<div class="error-404__div--inner">
 		<div class="error-404__div">
-			<p>You found Dino's hiding place!  Dino has been sitting here <i>all day</i> waiting for you to stop by.</p>
-			<p>Dino is a great hider, but you are clearly a better seeker.  You rock!</p>
+      <h2>You found Dino's hiding place!</h2>
+      <p>
+          Dino has been sitting here <em>all day</em> waiting for you to stop by.
+          Dino is a great hider, but you are clearly a better seeker.  You rock!
+      </p>
 		</div>
 
 		<div class="error-404__div--dino">
 		</div>
 	</div>
-</section>
 
-<section class="error-404">
-	<h2>Wait... what?!</h2>
-	<p>What do you mean you were looking for something else?  Are you <i>sure</i> you weren't looking for Dino?</p>
-	<p>Ok, fine.  Why don't you use this search box to find whatever could possibly be more important than a cute, cuddly baby dinosaur.  Just don't tell Dino.  That would make him sad.</p>
+  <div class="error-404__div--inner">
+    <h2>Wait... what?!</h2>
+    <p>
+      What do you mean you were looking for something else?!  Ok, fine.  Why
+      don't you use this search box to find whatever could possibly be more
+      important than a cute, cuddly baby dinosaur.  Just don't tell Dino.
+      That would make him sad.
+    </p>
 
-	<br />
+    <br />
 
-	<?php get_search_form(); ?>
+    <?php get_search_form(); ?>
+  </div>
 </section>
 
 <?php get_footer(); ?>
