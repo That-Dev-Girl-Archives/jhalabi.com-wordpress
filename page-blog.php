0db8fce3c515b6140ae5d90328dc3d36fd449837
Joni Halabi <her@thatdevgirl.com>
2018-01-23 20:18:00 +0000

    Editing the description on the blog home page.

--- page-blog.php
+++ page-blog.php
@@ -15,7 +15,7 @@ get_header(); ?>
 <div class="site-wrapper">
 	<header aria-label="Page header">
 		<h1><?php the_title(); ?></h1>
-		<p>Blog posts about life, photography, and whatever else.</p>
+		<p>Blog posts about the untechnical side of my life. For geekier posts, check out <a href="http://www.thatdevgirl.com/blog">my development blog</a>.</p>
 	</header>
 </div>
 
